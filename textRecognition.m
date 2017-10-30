3d2d5e13088a6f64642fd3cdecde6d796b23cf10
Nihil <eridusnihilxorn@gmail.com>
2017-10-30 21:10:12 +0300

    Update textRecognition.m

--- textRecognition.m
+++ textRecognition.m
@@ -1,7 +1,7 @@
 fileInput = input('Enter image-file name.\n', 's');
 %Preprocessing of image.
 I = imread(fileInput);
-%I = imrotate(I, 90); % Image has to be vertical or close to it.
+I = imrotate(I, 90); % Image has to be vertical or close to it.
 I = rgb2gray(I);
 
 %Calling for OCR in Computer Vision Toolbox and writing words to file.
